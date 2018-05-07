574071e8139db2361e9248e74c64a4ff0716e1cf
Pavel Khritonenko <pavel@hritonenko.ru>
2018-05-07 13:14:19 +0300

    Grpc project added

---
+++ src/Lykke.Common.ExchangeAdapter.Grpc/Extensions.cs
@@ -0,0 +1,33 @@
+﻿namespace Lykke.Common.ExchangeAdapter.Grpc
+{
+    public static class ProtoExtensions
+    {
+        public static System.DateTime ToSystem(this DateTime dateTime)
+        {
+            return System.DateTime.SpecifyKind(new System.DateTime((long)dateTime.Ticks), System.DateTimeKind.Utc);
+        }
+
+        public static DateTime ToProto(this System.DateTime dateTime)
+        {
+            return new DateTime { Ticks = (ulong)dateTime.Ticks };
+        }
+
+        public static System.Decimal ToSystem(this Decimal dec)
+        {
+            return new System.Decimal(new [] { dec.Lo, dec.Mid, dec.Hi, dec.SignScale });
+        }
+
+        public static Decimal ToProto(this System.Decimal dec)
+        {
+            var bits = System.Decimal.GetBits(dec);
+
+            return new Decimal
+            {
+                Lo = bits[0],
+                Mid = bits[1],
+                Hi = bits[2],
+                SignScale = bits[3]
+            };
+        }
+    }
+}
