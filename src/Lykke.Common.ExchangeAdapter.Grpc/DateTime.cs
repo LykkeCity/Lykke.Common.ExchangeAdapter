d3161e70c41678010338be6b50f8e12c6a907310
Pavel Khritonenko <pavel@hritonenko.ru>
2018-05-11 17:46:20 +0300

    Common structure implicit casting to system structures (Decimal / DateTime)

---
+++ src/Lykke.Common.ExchangeAdapter.Grpc/DateTime.cs
@@ -0,0 +1,17 @@
+﻿using Lykke.Common.Proto;
+
+namespace Lykke.Common.Proto
+{
+    public partial class DateTime
+    {
+        public static implicit operator System.DateTime(DateTime dateTime)
+        {
+            return System.DateTime.SpecifyKind(new System.DateTime((long)dateTime.Ticks), System.DateTimeKind.Utc);
+        }
+
+        public static implicit operator DateTime(System.DateTime dateTime)
+        {
+            return new DateTime { Ticks = (ulong)dateTime.Ticks };
+        }
+    }
+}
---
+++ src/Lykke.Common.ExchangeAdapter.Grpc/Decimal.cs
@@ -0,0 +1,23 @@
+﻿namespace Lykke.Common.Proto
+{
+    public partial class Decimal
+    {
+        public static implicit operator System.Decimal(Decimal dec)
+        {
+            return new System.Decimal(new [] { dec.Lo, dec.Mid, dec.Hi, dec.SignScale });
+        }
+
+        public static implicit operator Decimal(System.Decimal dec)
+        {
+            var bits = System.Decimal.GetBits(dec);
+
+            return new Decimal
+            {
+                Lo = bits[0],
+                Mid = bits[1],
+                Hi = bits[2],
+                SignScale = bits[3]
+            };
+        }
+    }
+}
