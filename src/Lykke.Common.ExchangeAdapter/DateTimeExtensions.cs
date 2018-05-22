6b7b72c328b024d550b02552f1ce505d4141ef11
Pavel Khritonenko <pavel@hritonenko.ru>
2018-05-22 15:15:27 +0300

    Epoch extension (ToUnixTimestamp but seconds based), EpochNonce class

---
+++ src/Lykke.Common.ExchangeAdapter/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lykke.Common.ExchangeAdapter
+{
+    public static class DateTimeExtensions
+    {
+        private static readonly DateTime BaseDateTime =
+            DateTime.SpecifyKind(new DateTime(1970, 1, 1), DateTimeKind.Utc);
+
+        public static long Epoch(this DateTime dt)
+        {
+            return (long) (dt - BaseDateTime).TotalSeconds;
+        }
+
+        public static DateTime FromEpoch(this long timestamp)
+        {
+            return BaseDateTime.AddSeconds(timestamp);
+        }
+    }
+}
---
+++ src/Lykke.Common.ExchangeAdapter/EpochNonce.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lykke.Common.ExchangeAdapter
+{
+    public sealed class EpochNonce : IDisposable
+    {
+        private static readonly ConcurrentDictionary<string, EpochNonce> Pool = new ConcurrentDictionary<string, EpochNonce>();
+
+        public static Task<T> Lock<T>(
+            string userId,
+            Func<long, Task<T>> code)
+        {
+            var nonce = Pool.GetOrAdd(userId, _ => new EpochNonce());
+            return nonce.Lock(code);
+        }
+
+        private readonly SemaphoreSlim _nonceLock = new SemaphoreSlim(1);
+
+        private long _current;
+
+        private async Task<T> Lock<T>(
+            Func<long, Task<T>> code)
+        {
+            await _nonceLock.WaitAsync();
+
+            try
+            {
+                var epoch = DateTime.UtcNow.Epoch();
+
+                if (epoch > _current)
+                {
+                    _current = epoch;
+                }
+                else
+                {
+                    _current++;
+                }
+
+                return await code(_current);
+            }
+            finally
+            {
+                _nonceLock.Release();
+            }
+        }
+
+        public void Dispose()
+        {
+            _nonceLock?.Dispose();
+        }
+    }
+}
