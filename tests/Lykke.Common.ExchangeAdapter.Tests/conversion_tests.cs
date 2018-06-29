207e1499d69e20e4d8508a6dcd807b1d100555cb
Pavel Khritonenko <pavel@hritonenko.ru>
2018-06-29 17:31:45 +0300

    Empty orderbook could be converted to tickprice with zero ask/bid (test)

---
+++ tests/Lykke.Common.ExchangeAdapter.Tests/conversion_tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Lykke.Common.ExchangeAdapter.Contracts;
+using NUnit.Framework;
+
+namespace Lykke.Common.ExchangeAdapter.Tests
+{
+    public class conversion_tests
+    {
+        [Test]
+        public void tickprice_from_empty_orderbook()
+        {
+            var ts = DateTime.UtcNow;
+
+            var ob = new OrderBook("source", "asset", ts, new OrderBookItem []{}, new OrderBookItem[]{});
+
+            var tp = TickPrice.FromOrderBook(ob);
+
+            Assert.AreEqual(ts, tp.Timestamp);
+            Assert.AreEqual("source", tp.Source);
+            Assert.AreEqual("ASSET", tp.Asset);
+            Assert.AreEqual(0M, tp.Ask);
+            Assert.AreEqual(0M, tp.Bid);
+        }
+    }
+}
